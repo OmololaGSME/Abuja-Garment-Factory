d0adbcc3daf7f65fe45cf37268f7536a8496b869
DBasic-cmd <oekisola589@gmail.com>
2024-06-02 23:44:52 +0100

    added the product page

--- apps/frontend/src/App.jsx
+++ apps/frontend/src/App.jsx
@@ -1,12 +1,25 @@
+
 import Routing from "./Routing"
+import Products from "./pages/Products/products"
 
 function App() {
+
+ {/* React.useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 800,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+    AOS.refresh();
+  }, []); */}
 
   return (
     <>
       <Routing />
+      <Products />
     </>
   )
 }
 
-export default App
+export default App;
