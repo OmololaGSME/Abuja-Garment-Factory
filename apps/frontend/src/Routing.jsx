ec4ae7c88da10b3ef294320eb6ce2423debb1bc8
Thomas Jennifer <jsamuelt13@gmail.com>
2024-06-22 13:01:22 +0100

    created admin pages

--- apps/frontend/src/Routing.jsx
+++ apps/frontend/src/Routing.jsx
@@ -2,16 +2,31 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Test from './pages/Test'
 import HomePage from './pages/HomePage'
+import AdminPage from './pages/AdminPage'
+import ImageSavedPage from './pages/ImageSavedPage'
+import ProductsUploadPage from './pages/ProductsUploadPage'
+
 
 const Routing = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path='/' element={<Test />}></Route>
+                <Route path="/" element={<Test />}></Route>
             </Routes>
             <Routes>
-                <Route path='/home' element={<HomePage />}></Route>
+                <Route path="/home" element={<HomePage />}></Route>
             </Routes>
+            <Routes>
+                <Route path="/admin" element={<AdminPage />}></Route>
+            </Routes>
+            <Routes>
+                <Route path="/image" element={<ImageSavedPage />}></Route>
+            </Routes>
+            <Routes>
+                <Route path="/upload" element={<ProductsUploadPage />}></Route>
+            </Routes>
+           
+            
         </BrowserRouter>
     )
 }
---
+++ apps/frontend/src/pages/ImageSavedPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ImageSaved from '../Container/ImageSaved/ImageSaved'
+
+const ImageSavedPage = () => {
+  return (
+    <div>
+      <ImageSaved />
+    </div>
+  )
+}
+
+export default ImageSavedPage
